27f940aae302cebba17b40b9c0f0b1b60a6f98f5
satish30118 <satishmaurya30118@gmail.com>
2024-10-25 15:14:45 +0530

    fixes

--- src/app/auth/login/page.jsx
+++ src/app/auth/login/page.jsx
@@ -86,7 +86,7 @@ export default function Login() {
 
             <button
               type="submit"
-              className="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition duration-200"
+              className="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition duration-200" disabled={loading}
             >
               {loading ? "Wait..." : "LOGIN"}
             </button>
